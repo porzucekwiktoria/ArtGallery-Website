b62bb37c1f7fe9c3f04e17d5633af6e386cce0bf
Wiktoria P <porzucekwiktoria410@wp.pl>
2019-01-23 16:10:03 +0100

    Add new files

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129733664-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129733664-1');
+</script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPQJDLN');</script>
+<!-- End Google Tag Manager -->
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="app.css">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200" rel="stylesheet">
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://www.youtube.com/iframe_api">
+    <title>Art Gallery</title>
+
+    <script>
+        $(document).ready(function() { 
+
+            $('a[href^="#"]').on('click', function(event) {
+
+                var target = $( $(this).attr('href') );
+
+                 if( target.length ) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                    scrollTop: target.offset().top
+                    }, 1000);}
+            });
+
+        });
+    </script>
+
+    <script>
+        $(window).scroll(function() {
+            $(".slideanim").each(function(){
+                var pos = $(this).offset().top;
+                var winTop = $(window).scrollTop();
+             if (pos < winTop + 600) {
+                $(this).addClass("slide");}
+            });
+        });
+    </script>
+  </head>
+  <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPQJDLN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+    <nav id="mainNavbar" class="navbar navbar-dark navbar-expand-md py-0 fixed-top text-white">
+        <a href="#myPage" class="navbar-brand">UART</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navLinks">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#about" class="nav-link">ABOUT</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#pics" class="nav-link">PICSY</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#contact" class="nav-link">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div id="carouselExampleSlidesOnly" class="carousel" data-ride="carousel">
+        <div class="carousel-inner">
+
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="main_3.jpg" alt="First slide">
+            <div class="carousel-caption">
+                <p>GET INSPIRED</p>
+            </div> 
+          </div>
+
+          <div class="carousel-item">
+            <img class="d-block w-100" src="main_4.jpg" alt="Second slide">
+            <div class="carousel-caption">
+                <p>BE CREATIVE</p>
+            </div> 
+          </div>
+          
+        </div>
+    </div>
+
+      <div id="about">
+          <div class="content slideanim">
+                <p id="title" class="text-center">ABOUT</p>
+                <div class="container slideanim">
+                <p class="text-center"> Site is so hipster lorem ipsum. Lorem ipsum dolor amet truffaut vape pug austin retro blog raclette plaid. Vaporware actually activated charcoal marfa locavore. Prism enamel pin freegan art party, celiac gastropub kitsch asymmetrical flexitarian lomo narwhal schlitz bespoke. Lo-fi mlkshk fam shabby chic letterpress banh mi, brooklyn portland four loko flexitarian thundercats humblebrag umami pickled actually.
+                        Lomo messenger bag fixie knausgaard. Raclette palo santo ramps poke tattooed. Four loko ugh godard yr mlkshk brooklyn raclette.Swag tbh iPhone quinoa cray. Church-key offal gentrify listicle distillery everyday carry synth tumblr. Adaptogen hexagon pitchfork, cloud bread lumbersexual vegan offal chia before they sold out. Jianbing banh mi quinoa vexillologist fam iceland mumblecore organic whatever selfies trust fund skateboard vice tattooed portland. Single-origin coffee affogato kinfolk succulents retro, cronut pabst fashion axe. Cornhole literally pour-over, ugh pug unicorn food truck godard jianbing chartreuse cliche ramps sartorial fashion axe blog. +1 flexitarian banjo gastropub meggings gluten-free.
+                        Unicorn pork belly tousled, chia hella chambray salvia semiotics messenger bag bitters raw denim.Green juice church-key williamsburg forage portland plaid skateboard synth pickled DIY paleo fanny pack shoreditch. Air plant schlitz hashtag, glossier ugh fanny pack photo booth. Snackwave chartreuse single-origin coffee biodiesel tbh gastropub subway tile sriracha selfies meggings next level fingerstache. Microdosing skateboard green juice chartreuse, readymade 8-bit yr wayfarers stumptown ramps kickstarter ennui. Whatever cliche copper mug, chia palo santo brunch thundercats tbh shoreditch forage kombucha. Before they sold out 8-bit shaman man braid four dollar toast hot chicken seitan venmo jean shorts fam echo park gastropub pug unicorn. Umami +1 street art cronut knausgaard neutra asymmetrical irony. Synth knausgaard neutra tacos echo park, succulents art party banjo 8-bit cloud bread. Dreamcatcher biodiesel crucifix hexagon church-key green juice bushwick health goth pop-up ennui air plant fam hoodie celiac. Craft beer shabby chic church-key, listicle copper mug 8-bit cold-pressed semiotics lyft. 3 wolf moon brunch blue bottle, iPhone unicorn 8-bit meditation cronut keytar dreamcatcher XOXO lyft.
+                        Prism knausgaard franzen, cardigan tofu lomo godard post-ironic. Art party bitters williamsburg, roof party tattooed air plant tumblr meh. Mlkshk retro beard deep v man bun kinfolk vegan chambray la croix. Hella kinfolk 8-bit post-ironic banh mi polaroid. Banh mi portland aesthetic banjo. Vaporware yuccie occupy knausgaard vegan salvia disrupt ramps microdosing crucifix tacos mumblecore. Selvage farm-to-table 3 wolf moon, sustainable umami vegan artisan chia tofu. Ennui shaman disrupt, taxidermy fixie echo park williamsburg dreamcatcher sriracha chillwave literally hella flannel. Kale chips pork belly plaid, cray offal taxidermy single-origin coffee. Hashtag quinoa cardigan fanny pack mumblecore franzen. Blog semiotics typewriter flannel. </p>
+                        <a class="btn btn-outline-dark float-right" href="blog.html">MORE INFO</a>
+                    </div>
+            </div> 
+      </div>
+
+      <div id="pics">
+          <div class="content">
+              <div class="container">
+              <p id="title" class="text-center">PICSY</p>
+              <div class="slideanim" id="img_head">
+                    <img class="d-block w-100" src="body_1.jpg" alt="Egg">
+              </br>
+                    <p id="color" class="text-center">Bushwick umami flannel fixie, ramps waistcoat banjo semiotics cred keffiyeh deep v. Try-hard hoodie direct trade banh mi. Fanny pack chicharrones small batch 8-bit, pitchfork yr dreamcatcher tacos. Whatever 90's lumbersexual bushwick live-edge. Stumptown blue bottle raw denim forage disrupt.</p>
+              
+                    <a  class="btn btn-info float-right" href="https://www.definitions.net/definition/ART">What does it mean?</a>
+                    <a  class="btn btn-secondary float-right" href="https://en.wikipedia.org/wiki/Creativity">Creativity?</a>
+              </div>
+               
+                <div class="embed-responsive embed-responsive-16by9 mb-3">
+                  
+                    
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Xoe3Nn5vuBg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                   
+                </div>
+            <div class="container">
+              <p class="text-center">Check out <a id="pdf" href="art2.pdf" download="art2.pdf">Art of Drawing Human Body</a> and
+              <a id="pdf" href="art1.pdf" download="art1.pdf">Textbook of Engineering Drawing</a> !</p>
+            </div>
+           
+                <div class="row slideanim">
+                        <div class="col-lg-4 col-md-6 flex">
+                            
+                            <img class="img-thumbnail" src="body_6.jpg" alt="ph_1" >
+
+                        </div>
+                        <div class="col-lg-4 col-md-6 flex ">
+                            <img class="img-thumbnail" src="body_7.jpg" alt="ph_2" >
+                        
+                        </div>
+                        <div class="col-lg-4 col-md-6 flex">
+                            <img class="img-thumbnail" src="body_3.jpg" alt="ph_3">
+                        
+                        </div>
+                </div>
+            </br>
+                <div class="row slideanim">
+                        <div class="col-lg-4 col-md-6 flex">
+                            
+                            <img class="img-thumbnail" src="body_2.jpg" alt="ph_5" >
+                        
+                        </div>
+                        <div class="col-lg-4 col-md-6 flex ">
+                            <img class="img-thumbnail" src="body_8.jpg" alt="ph_6" >
+                        
+                        </div>
+                        <div class="col-lg-4 col-md-6 flex">
+                            <img class="img-thumbnail" src="body_4.jpg" alt="ph_7">
+                        
+                        </div>
+                </div>
+             </div>  
+          </div>
+            
+        </div>
+
+    <div id="contact" class="container">
+
+        <p id="title" class="text-center text-white">CONTACT</p>
+        <p class="text-center text-white"><em>Any questions? </br>Please, feel free to contact us</em></p>
+
+    <div class="row">
+
+        <div class="col-md-2"></div>    
+
+        <div class="col-md-8">
+
+          <form id="contact-form" method="post" action="index.php" role="form">
+
+            <div class="row slideanim">
+
+                <div class="col-sm-6 form-group">
+                    <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                </div>
+                <div class="col-sm-6 form-group">
+                    <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                </div>
+            </div>
+
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
+            
+      <br>
+
+      <div class="row">
+        <div class="col-md-12 form-group">
+          <button class="btn btn-outline-light btn-block" name="submitbutton" type="submit">Send</button>
+        </div>
+      </div>
+
+      </div>
+      <div class="col-md-2"></div>
+        <?php 
+
+        if (isset($_POST['name'])) {
+            $name = $_POST['name'];
+        }
+
+        if (isset($_POST['email'])) {
+            $email = $_POST['email'];
+        }
+
+        if (isset($_POST['comments'])) {
+            $comments = $_POST['comments'];
+        }
+
+    
+    if(isset($_POST['submitbutton'])){
+        if(!empty($name) and !empty($email) and !empty($comments)) { 
+     
+        $conn = mysqli_connect('localhost', 'agprojekt1', 'AGmilusia1','artgalleryprojekt1') 
+        or die('Brak połączenia z baza MySQL'); 
+        $sql = "INSERT INTO test (name, email, comments)
+                VALUES ('$name', '$email', '$comments')";
+
+        if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        } 
+
+        else {echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+    $conn->close();
+     
+        }   
+        }
+        ?>    
+
+</form>
+    </div>
+    </div>
+
+
+  <!-- Footer -->
+    <footer class="card-footer">
+
+        <!-- Copyright -->
+        <div class="text-center">© 2018 Copyright
+          <a href="mailto:porzucekwiktoria410@gmail.com"> Porzucek Wiktoria</a>
+        </div>
+        <!-- Copyright -->
+      
+    </footer>
+      <!-- Footer -->
+
+    
+    
+    <script>
+        $(function () {
+            $(document).scroll(function () {
+                var $nav = $("#mainNavbar");
+                $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+            });
+        });
+    </script>
+
+    
+    
+        
+</body>
+</html>
